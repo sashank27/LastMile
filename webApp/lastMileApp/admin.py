5af048475f7f83f742d76953911d1c0330d1b62a
Shreyansh Dwivedi <shreyanshdwivedi1997@gmail.com>
2019-01-26 03:14:38 +0530

    Changes to model in djangoApp and added test.json

--- webApp/lastMileApp/admin.py
+++ webApp/lastMileApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Passenger, Booking, Flight, FlightCheck
 
 # Register your models here.
+admin.site.register(Passenger)
+admin.site.register(Booking)
+admin.site.register(Flight)
+admin.site.register(FlightCheck)
--- webApp/lastMileApp/models.py
+++ webApp/lastMileApp/models.py
@@ -1,29 +1,34 @@
 from django.db import models
+from datetime import datetime    
 
 class Passenger(models.Model):
+    passenger_id      = models.AutoField(primary_key=True, default=1)
     fname             = models.CharField(max_length=255, null=True)
     lname             = models.CharField(max_length=255, null=True)
     age               = models.IntegerField()
     email             = models.CharField(max_length=200)
-    phoneNumber       = models.IntegerField()
+    phoneNumber       = models.CharField(max_length=200)
     gender            = models.CharField(max_length=20)
     checkedBags       = models.IntegerField()
     classType         = models.CharField(max_length=100)
     specialPreference = models.BooleanField(default=False)
+    bookingRef        = models.CharField(max_length=200, null=True)
     image             = models.CharField(max_length=500, null=True)
 
     def __str__(self):
         return self.fname + self.lname
 
 class Booking(models.Model):
-    passenger = models.ForeignKey(Passenger, on_delete=models.CASCADE)
-    flightID  = models.CharField(max_length=200)
+    booking_id = models.AutoField(primary_key=True, default=1)
+    passenger  = models.ForeignKey(Passenger, on_delete=models.CASCADE)
+    flightID   = models.CharField(max_length=200)
 
 class Flight(models.Model):
+    flight_id        = models.AutoField(primary_key=True, default=1)
     airline          = models.CharField(max_length=200)
     flightNumber     = models.CharField(max_length=200)
-    arrivalTime      = models.CharField(max_length=200)
-    departureTime    = models.CharField(max_length=200)
+    arrival          = models.DateTimeField(default=datetime.now, blank=True)
+    departure        = models.DateTimeField(default=datetime.now, blank=True)
     arrivalAirport   = models.CharField(max_length=200)
     departureAirport = models.CharField(max_length=200)
     status           = models.CharField(max_length=200)
