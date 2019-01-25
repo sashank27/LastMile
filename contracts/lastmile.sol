cc9ae772f5f4c3f5234e1914e6a8acdb5f8ada26
sai <saicharan.reddy1@gmail.com>
2019-01-26 01:37:04 +0530

    Basic contracts

---
+++ contracts/lastmile.sol
@@ -0,0 +1,58 @@
+
+contract lastmile{
+
+    struct Passenger{
+        bytes32 fname;
+        bytes32 lname;
+        uint age;
+        bytes32 gender;
+        uint checked_bags;
+        bytes32 special_preferences;
+        bytes32 class;
+
+    }
+
+    struct Booking{
+        Passenger[] passengers;
+        Flight flight;
+
+    }
+
+    struct Flight{
+        bytes32 airline;
+        bytes32 flight_number;
+        uint departure_time;
+        uint arrival_time;
+        bytes32 departure_airport;
+        bytes32 arrival_airport;
+        bytes32 status;
+        Flight_check flight_check;
+
+    }
+
+    struct Flight_check{
+        bool hydraulics;
+        bool wheels;
+        bool landing_mechanism;
+        bool nav_system;
+    }
+
+    mapping (address => Passenger) passengers;
+    address[] public PassengersAccts;
+
+
+    function setPassenger (address _address, bytes32 _fname, bytes32 _lname, uint _age, bytes32 _gender,
+                            uint _checked_bags, bytes32 _special_preferences, bytes32 _class) view public {
+        var passenger = passengers[_address];
+
+        passenger.fname = _fname;
+        passenger.lname = _lname;
+        passenger.age = _age;
+        passenger.gender = _gender;
+        passenger.checked_bags = _checked_bags;
+        passenger.special_preferences = _special_preferences;
+        passenger.class = _class;
+
+    }
+
+}
